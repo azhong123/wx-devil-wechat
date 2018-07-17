92715a5e6ebe12a052854daf401fdd40344ecc17
azhong123 <194953605@qq.com>
2018-07-17 22:21:21 +0800

    完成tabbar功能，主题详情[C

---
+++ pages/theme/theme-model.js
@@ -0,0 +1,25 @@
+import {
+  Base
+} from '../../utils/base.js';
+class Theme extends Base {
+  constructor() {
+    super();
+  }
+
+  /**
+   * 获取主题详情
+   */
+  getThemeDetail(id, callback) {
+    var params = {
+      url: "/index/v1/theme/one/" + id,
+      sCallback: function(res) {
+        console.log(res);
+        callback && callback(res);
+      }
+    };
+    this.request(params);
+  }
+}
+export {
+  Theme
+};
--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,18 +1,43 @@
 // pages/theme/theme.js
+import {
+  Theme
+} from 'theme-model.js';
+var theme = new Theme;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var id = options.id;
-    console.log(id);
+  onLoad: function(options) {
+    this.data.id = options.id;
+    this._loadData();
+  },
+
+  /**
+   * 获取主题详情
+   */
+  _loadData: function() {
+    theme.getThemeDetail(this.data.id, (res) => {
+      this.setData({
+        "themeDetail": res
+      });
+    });
+  },
+
+  /**
+   * 跳转 商品详情页
+   */
+  onProductsItemTap: function(event) {
+    var id = theme.getDataSet(event, "id");
+    wx.navigateTo({
+      url: '../product/product?id=' + id,
+    })
   }
 })
