b62482e96154e7f218e06b71c49e236541537339
azhong123 <194953605@qq.com>
2018-07-19 22:43:57 +0800

    商品详情页添加轮播 数据显示，未完成页面交互

--- pages/product/product.js
+++ pages/product/product.js
@@ -9,7 +9,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id: null
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: false, //是否自动切换
+    duration: 1000, //  滑动动画时长1s
+    id: null,
+    imgUrls: [],
   },
 
   /**
@@ -26,6 +30,7 @@ Page({
      */
     product.getGoodsDetail(this.data.id, (res) => {
       console.log(res);
+      this.data.imgUrls = res.mainImgUrls;
       this.setData({
         'productDetail': res
       });
@@ -50,6 +55,26 @@ Page({
         'productParamItems': res
       });
     });
+  },
+
+  /**
+   * 图片放大预览
+   */
+  previewImage: function(res) {
+    console.log(res);
+    var imgSrc = product.getDataSet(res, "src");
+    console.log(imgSrc);
+    wx.previewImage({
+      current: imgSrc, // 当前显示图片的http链接  
+      urls: this.data.imgUrls // 需要预览的图片http链接列表  
+    })
+  },
+  
+  /**
+   * 选择规格和数量
+   */
+  toggleDialog: function(res){
+
   }
 
 
